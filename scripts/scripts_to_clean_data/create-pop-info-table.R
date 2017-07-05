db347f0e0ff6d0f54642ba9cb959d814c6e758ae
erwhite1 <eastonrwhite@gmail.com>
2017-07-05 10:39:35 -0700

    edited file to only include most 35 recent years of each time series

--- scripts/scripts_to_clean_data/create-pop-info-table.R
+++ scripts/scripts_to_clean_data/create-pop-info-table.R
@@ -39,24 +39,30 @@ pop_info$autocorrelation= NA
 pop_info$trend_p_value=NA
 pop_info$min_time_for_power=NA
 pop_info$popvalue=NA
-long_dat$pop_value=long_dat$pop_value
+time_series_data=NULL
 source('scripts/calculate_slope.R')
   for (j in 1:nrow(pop_info)){
     pop= subset(long_dat,long_dat$ID==pop_info$ID[j])
     pop = pop[(nrow(pop)-34):nrow(pop),]
     pop$popvalue=as.numeric(pop$popvalue)
     pop$popvalue=pop$popvalue/max(pop$popvalue) #should I standaridize this in a better way
-    # pop_info$variance[j]= var(pop$popvalue)
-    # pop_info$overall_trend[j] = calculate_slope(pop$popvalue)
-    # pop_info$autocorrelation[j] = acf(pop$popvalue,plot = FALSE)$acf[2]
-    # pop_info$trend_p_value[j]=calculate_p_value(pop$popvalue)
-    # pop_info$popvalue = pop$popvalue[1] #initial abundance
-    # pop_info$series_length = nrow(pop)
+    pop_info$year[j] = pop$year[1]
+    pop_info$variance[j]= var(pop$popvalue)
+    pop_info$overall_trend[j] = calculate_slope(pop$popvalue)
+    pop_info$autocorrelation[j] = acf(pop$popvalue,plot = FALSE)$acf[2]
+    pop_info$trend_p_value[j]=calculate_p_value(pop$popvalue)
+    pop_info$popvalue = pop$popvalue[1] #initial abundance
+    pop_info$series_length = nrow(pop)
     
-   source('scripts/calculate_power_metric.R')
+   #source('scripts/calculate_power_metric.R')
      pop_info$min_time_for_power[j] = min_time_needed(pop$popvalue,0.05,0.8)
     print(paste(j,':',pop_info$min_time_for_power[j],sep=' '))
+    
+    
+    time_series_data = rbind(time_series_data,pop)
   }
+
+long_dat = time_series_data
 
 #IUCN calculation
 pop_info$IUCN = 3*pop_info$gen_len
